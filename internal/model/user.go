7605b9999383f39340d8555ef1296f53e70d14ec
lemavisaitov <visaitovlema@gmail.com>
2025-03-06 21:12:48 +0300

    refactor: add ID to UpdateUserRequest

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,15 +3,16 @@ package model
 import "github.com/google/uuid"
 
 type User struct {
-	ID       uuid.UUID `json:"id"`
-	Login    string    `json:"login"`
-	Password string    `json:"email"`
-	Name     string    `json:"name"`
-	Age      int       `json:"age"`
+	ID       uuid.NullUUID `json:"id"`
+	Login    string        `json:"login"`
+	Password string        `json:"email"`
+	Name     string        `json:"name"`
+	Age      int           `json:"age"`
 }
 
 type UpdateUserRequest struct {
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int    `json:"age"`
+	ID       uuid.UUID `json:"id"`
+	Password string    `json:"password"`
+	Name     string    `json:"name"`
+	Age      int       `json:"age"`
 }
