56c5886346d404131bd84717dc8eb0246b8a9f1a
lemavisaitov <visaitovlema@gmail.com>
2025-03-08 16:23:16 +0300

    fix .gitignore

--- migration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source .env
-
-export MIGRATION_DSN="host=postgres port=5432 user=${DB_USER} password=${DB_PASSWORD} sslmode=disable"
-
-goose -dir "./migrations" postgres "${MIGRATION_DSN}" up -v
