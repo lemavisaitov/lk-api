9fcc6d1971fdb46301a3f87193fb081e468611b3
lemavisaitov <visaitovlema@gmail.com>
2025-03-09 22:45:26 +0300

    refactor & fix

--- cmd/main.go
+++ cmd/main.go
@@ -2,30 +2,42 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/lemavisaitov/lk-api/internal/app"
+	"github.com/lemavisaitov/lk-api/internal/cache"
+	"github.com/lemavisaitov/lk-api/internal/config"
 	"github.com/lemavisaitov/lk-api/internal/handler"
+	"github.com/lemavisaitov/lk-api/internal/repository"
 	"github.com/lemavisaitov/lk-api/internal/storage"
+	"github.com/lemavisaitov/lk-api/internal/usecase"
+	"github.com/lemavisaitov/lk-api/migrations"
 	"log"
-	"time"
 )
 
 func main() {
-	connStr := "host=postgres port=5432 user=postgres database=postgres password=postgres sslmode=disable"
+	cfg := config.MustLoad()
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s",
+		cfg.DBUser, cfg.DBPassword, cfg.DBHost, "5432")
 
 	ctx := context.Background()
-	withTimeout, cancel := context.WithTimeout(ctx, 5*time.Second)
+	withTimeout, cancel := context.WithTimeout(ctx, cfg.DBConnTimeout)
 	defer cancel()
 
-	s, err := storage.GetConnect(withTimeout, connStr)
+	pool, err := storage.GetConnect(withTimeout, connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
+	if err := migrations.Migrate(connStr); err != nil {
+		log.Fatal(err)
+	}
+	userRepo := repository.NewUserProvider(pool)
+	cacheProvider := cache.NewDecorator(userRepo, cfg.CacheInterval, cfg.CacheTTL)
+	userUC := usecase.NewUserProvider(cacheProvider)
+	handle := handler.New(userUC)
+	router := app.GetRouter(handle)
 
-	h := handler.New(s)
-	router := app.GetRouter(h)
-
-	err = router.Run(":8080")
-	if err != nil {
+	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
 }
